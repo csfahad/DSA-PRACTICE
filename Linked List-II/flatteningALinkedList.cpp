0aa3f0d090296140648a9e15c7b6d3c1126e5507
Cs Fahad <csfahad.officiall@gmail.com>
2023-09-25 21:25:31 +0530

    LinkedList-II/flatteningALinkedList

---
+++ Linked List-II/flatteningALinkedList.cpp
@@ -0,0 +1,55 @@
+// Flattening a Linked List
+
+// PROBLEM LINKS:
+// GFG: https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/1
+// CODE STUDIO: https://www.codingninjas.com/studio/problems/1112655
+
+
+// INTUTION: USING MERGE ALGO OF MERGE SORT
+
+// Time Complexity: O(N), where N is the total number of nodes present
+// Reason: We are visiting all the nodes present in the given list.
+// Space Complexity: O(1)
+
+
+Node* mergeTwoLists(Node* a, Node* b) {
+    
+    Node *temp = new Node(0);
+    Node *res = temp; 
+    
+    while(a != NULL && b != NULL) {
+        if(a->data < b->data) {
+            temp->bottom = a; 
+            temp = temp->bottom; 
+            a = a->bottom; 
+        }
+        else {
+            temp->bottom = b;
+            temp = temp->bottom; 
+            b = b->bottom; 
+        }
+    }
+    
+    if(a) temp->bottom = a; 
+    else temp->bottom = b; 
+    
+    return res -> bottom; 
+    
+}
+
+
+Node *flatten(Node *root) {
+   
+        if (root == NULL || root->next == NULL) 
+            return root; 
+  
+        // recur for list on right 
+        root->next = flatten(root->next); 
+  
+        // now merge 
+        root = mergeTwoLists(root, root->next); 
+  
+        // return the root 
+        // it will be in turn merged with its left 
+        return root; 
+}
