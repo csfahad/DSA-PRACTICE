4f80de79685ab50e5ff061ee879ef9fe7b97f35a
Cs Fahad <csfahad.officiall@gmail.com>
2023-09-25 18:52:03 +0530

    LinkedList-II/checkLLIsPalindromeOrNot

---
+++ Linked List-II/checkLLIsPalindromeOrNot.cpp
@@ -0,0 +1,149 @@
+// Check if given Linked List is Plaindrome
+
+// PROBLEM LINKS:
+// LEETCODE: https://www.codingninjas.com/studio/problems/799352
+// CODE STUDIO: https://leetcode.com/problems/palindrome-linked-list/
+
+
+// BRUTEFORCE APPROACH : USING THE EXTRA DATA STRUCTURE
+
+// Time Complexity: O(N)
+// Reason: Iterating through the list to store elements in the array.
+
+// Space Complexity: O(N)
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node {
+    public:
+        int num;
+        node* next;
+        node(int val) {
+            num = val;
+            next = NULL;
+        }
+};
+
+void insertNode(node* head,int val) {
+    node* newNode = new node(val);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+
+bool isPalindrome(node* head) {
+    vector<int> arr;
+    while(head != NULL) {
+        arr.push_back(head->num);
+        head = head->next;
+    }
+    for(int i=0;i<arr.size()/2;i++) 
+        if(arr[i] != arr[arr.size()-i-1]) return false;
+    return true;
+}
+
+int main() {
+    node* head = NULL;
+    insertNode(head,1);
+    insertNode(head,2);
+    insertNode(head,3);
+    insertNode(head,2);
+    insertNode(head,1);
+    isPalindrome(head)? cout<<"True" : cout<<"False";
+    return 0;
+}
+
+
+
+
+// OPTIMAL APPROACH: USING SLOW & FAST POINTER
+
+// Time Complexity: O(N/2)+O(N/2)+O(N/2)
+// Reason: O(N/2) for finding the middle element, reversing the list from the middle element, 
+// and traversing again to find palindrome respectively.
+
+// Space Complexity: O(1)
+
+
+#include<iostream>
+using namespace std;
+
+class node {
+    public:
+        int num;
+        node* next;
+        node(int val) {
+            num = val;
+            next = NULL;
+        }
+};
+
+void insertNode(node* head,int val) {
+    node* newNode = new node(val);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    
+    node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+
+node* reverse(node* ptr) {
+    node* pre=NULL;
+    node* nex=NULL;
+    while(ptr!=NULL) {
+        nex = ptr->next;
+        ptr->next = pre;
+        pre=ptr;
+        ptr=nex;
+    }
+    return pre;
+}
+
+bool isPalindrome(node* head) {
+    if(head==NULL||head->next==NULL) return true;
+        
+    node* slow = head;
+    node* fast = head;
+        
+    while(fast->next!=NULL&&fast->next->next!=NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+        
+    slow->next = reverse(slow->next);
+    slow = slow->next;
+    node* dummy = head;
+        
+    while(slow!=NULL) {
+        if(dummy->num != slow->num) return false;
+        dummy = dummy->next;
+        slow = slow->next;
+    }
+    return true;
+}
+
+int main() {
+    node* head = NULL;
+    insertNode(head,1);
+    insertNode(head,2);
+    insertNode(head,3);
+    insertNode(head,2);
+    insertNode(head,1);
+    isPalindrome(head)? cout<<"True" : cout<<"False";
+    return 0;
+}
