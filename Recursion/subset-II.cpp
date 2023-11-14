851f3723159883a8689c63fb1d3b856da51df33b
Cs Fahad <csfahad.officiall@gmail.com>
2023-11-14 18:54:33 +0530

    Recursion/subset-II

---
+++ Recursion/subset-II.cpp
@@ -0,0 +1,125 @@
+// Subset – II | Print all the Unique Subsets
+
+// PROBLEM LINKS:
+// LEETCODE: https://leetcode.com/problems/subsets-ii/
+// CODE STUDIO: https://www.codingninjas.com/studio/problems/unique-subsets_3625236
+
+// BRUTEFORCE APPROACH
+
+// At every index, we make a decision whether to pick or not pick the element at that index. 
+// This will help us in generating all possible combinations but does not take care of the duplicates. 
+// Hence we will use a set to store all the combinations that will discard the duplicates.
+
+// Time Complexity: O( 2^n *(k log (x) )). 2^n  for generating every subset and k* log( x)  to 
+// insert every combination of average length k in a set of size x. After this, we have to convert 
+// the set of combinations back into a list of list /vector of vectors which takes more time.
+
+// Space Complexity:  O(2^n * k) to store every subset of average length k. 
+// Since we are initially using a set to store the answer another O(2^n *k) is also used.
+
+
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+void printAns(vector < vector < int >> & ans) {
+  cout << "The unique subsets are " << endl;
+  cout << "[ ";
+  for (int i = 0; i < ans.size(); i++) {
+    cout << "[ ";
+    for (int j = 0; j < ans[i].size(); j++)
+      cout << ans[i][j] << " ";
+    cout << "]";
+  }
+  cout << " ]";
+}
+
+class Solution {
+  public:
+    void fun(vector < int > & nums, int index, vector < int > ds, set < vector < int >> & res) {
+      if (index == nums.size()) {
+        sort(ds.begin(), ds.end());
+        res.insert(ds);
+        return;
+      }
+      ds.push_back(nums[index]);
+      fun(nums, index + 1, ds, res);
+      ds.pop_back();
+      fun(nums, index + 1, ds, res);
+    }
+  vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+    vector < vector < int >> ans;
+    set < vector < int >> res;
+    vector < int > ds;
+    fun(nums, 0, ds, res);
+    for (auto it = res.begin(); it != res.end(); it++) {
+      ans.push_back( * it);
+    }
+    return ans;
+  }
+};
+
+int main() {
+  Solution obj;
+  vector < int > nums = {1, 2, 2};
+  vector < vector < int >> ans = obj.subsetsWithDup(nums);
+  printAns(ans);
+  return 0;
+}
+
+
+
+// OPTIMAL APPROACH:
+
+// Time Complexity: O(2^n) for generating every subset and O(k)  to insert every subset 
+// in another data structure if the average length of every subset is k. Overall O(k * 2^n).
+
+// Space Complexity: O(2^n * k) to store every subset of average length k. 
+// Auxiliary space is O(n)  if n is the depth of the recursion tree.
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void printAns(vector < vector < int >> & ans) {
+   cout<<"The unique subsets are "<<endl;
+   cout << "[ ";
+   for (int i = 0; i < ans.size(); i++) {
+      cout << "[ ";
+      for (int j = 0; j < ans[i].size(); j++)
+         cout << ans[i][j] << " ";
+      cout << "]";
+   }
+   cout << " ]";
+}
+
+class Solution {
+   private:
+      void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
+         ans.push_back(ds);
+         for (int i = ind; i < nums.size(); i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            ds.push_back(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.pop_back();
+         }
+      }
+   public:
+      vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+         vector < vector < int >> ans;
+         vector < int > ds;
+         sort(nums.begin(), nums.end());
+         findSubsets(0, nums, ds, ans);
+         return ans;
+      }
+};
+
+int main() {
+   Solution obj;
+   vector < int > nums = {1,2,2 };
+   vector < vector < int >> ans = obj.subsetsWithDup(nums);
+   printAns(ans);
+   return 0;
+}
